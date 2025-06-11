fd790ce0ae804b84e5d575259f7ebaae7a706e92
Zane1732 <112121784+Zane1732@users.noreply.github.com>
2025-06-11 17:32:29 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 
 import { Link } from 'react-router-dom';
-import { ExternalLink, Github, Twitter, Facebook, Instagram, Mail } from 'lucide-react';
+import { ExternalLink, Github,Instagram, Mail } from 'lucide-react';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
